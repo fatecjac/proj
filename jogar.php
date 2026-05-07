9d36ef3cce5445e83d05cbcb949825188a20cba2
fatecjac <fatecjac@gmail.com>
2026-05-07 16:30:13 +0000

    proj v1

---
+++ jogar.php
@@ -0,0 +1,9 @@
+<?php
+ echo "<h1><pre>De que seriado é o personagem 
+      Nino da tv cultura?</pre></h1>";
+ echo "<h1>a) Oi</pre></h1>";
+ echo "<h1>b) Culturinha</pre></h1>";
+ echo "<h1>c) Castelo RaTimBum</h1>";
+ echo "<h1>Resposta correta: C</h1>";
+ 
+?>
---
+++ jogo.php
@@ -0,0 +1,7 @@
+<?php
+ $J1 = $_POST['n1'];
+ $J2 = $_POST['n2'];
+ $J3 = $_POST['n3'];
+ echo "<h1>$J1, $J2 e $J3, prepares?<h1>";
+ echo "<h1><a href=jogar.php>Jogar</a></h1>";
+?>
